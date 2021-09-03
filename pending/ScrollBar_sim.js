1da1fddfdf2d2e36ccede556d3c17f33b032063a
Xiaosong Wen <xwen2@wpi.edu>
2021-09-03 08:53:44 -0400

    XIaoosng

---
+++ pending/ScrollBar_sim.js
@@ -0,0 +1,103 @@
+class ScrollBar01{
+    constructor(name, renderer, isVertical, cellSize, usingCache=true){
+        this.name = name;
+        this.bar = document.querySelector(name);
+        this.ticker = this.bar.querySelector(".ticker");
+        this.tableRenderer = renderer;
+        this.isVertical = isVertical;
+        this.cellSize = cellSize;
+
+        this.mouseMoveHandler = this.mouseMove.bind(this);
+        this.bar.onmousedown = this.onMouseDown.bind(this);
+        this.bar.onclick = this.onclick.bind(this);
+        this.remmoveListenerHander = this.remmoveListener.bind(this);
+
+        if (isVertical){
+            this.wheelMoveHandler = this.wheelMove.bind(this);
+        }
+        
+        this.tickerPrePos = 0;
+
+        this.usingCache = usingCache
+        // if (usingCache){
+        //     this.UpDateTicker();
+        // }
+    }
+    // UpDateTicker(){
+    //     let cache = this.tableRenderer.cache;
+        
+    //     if (this.isVertical){
+    //         let end = cache.cached.endRow;
+    //         let tickerSize = cache.totalRowsToDisplay / end;
+    //         let tickerPos = cache.windowDisplay.startVisibleRow / end;
+    //         this.ticker.style.height = this.bar.clientHeight * tickerSize + "px";
+    //         this.ticker.style.top = this.bar.clientHeight * tickerPos + "px";
+    //     }else{
+    //         let end = cache.cached.endCol;
+    //         let tickerSize = cache.totalColsToDisplay / end;
+    //         let tickerPos = cache.windowDisplay.startVisibleCol / end;
+    //         this.ticker.style.width = this.bar.clientWidth * tickerSize + "px";
+    //         this.ticker.style.left = this.bar.clientWidth * tickerPos + "px";
+    //     }
+    // }
+    mouseMove(e){
+        if (this.isVertical){
+            this.setTickerPos(e.clientY-50);
+        }else{
+            this.setTickerPos(e.clientX-20);
+        }
+        e.preventDefault();
+    }
+    onclick(e){
+        if (this.isVertical){
+            this.setTickerPos(e.clientY-50);
+        }else{
+            this.setTickerPos(e.clientX-20);
+        }
+    }
+    onMouseDown(){
+        this.bar.addEventListener("mousemove", this.mouseMoveHandler, false);
+    }
+    onMouseUp(){
+        this.bar.removeEventListener("mousemove", this.mouseMoveHandler, false);
+    }
+    remmoveListener(){
+        this.onMouseUp();
+    }
+    wheelMove(dy){        
+        this.setTickerPos(this.ticker.offsetTop+dy)
+    }
+    auxChange(d){
+        // if (this.isVertical){
+            
+        //     console.log("y",d)
+            
+        //     // this.setTickerPos(this.tickerPrePos + d);
+        // }else{
+        //     console.log("x", d)
+            
+        // }
+        // this.setTickerPos(this.tickerPrePos + d);
+    }
+    setTickerPos(pos){
+        pos = Math.max(0, pos);
+        let max = 0;
+        if (this.isVertical){
+            max = this.bar.clientHeight - this.ticker.clientHeight;
+            pos = Math.min(pos, max);
+            this.ticker.style.top = pos + "px";
+        }else{
+            max = this.bar.clientWidth - this.ticker.clientWidth;
+            pos = Math.min(pos, max);
+            this.ticker.style.left = pos + "px";
+        }
+        // let percentage = pos / max;
+        
+        if (this.usingCache){
+            this.tableRenderer.virtualrenderTable(pos/max, this.isVertical);       
+            this.tickerPrePos = pos;
+        }else{
+            this.tableRenderer.renderTable(pos / max, this.isVertical);
+        }
+    }
+}
